cd64bb2221ca22d6728d2c1e6887f8698128e6dc
johannroa <js.roa10@uniandes.edu.co>
2022-07-21 21:15:01 -0500

    Prueba

---
+++ src/test/java/co/edu/usa/prueba/AppPrueba/AppPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.prueba.AppPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
